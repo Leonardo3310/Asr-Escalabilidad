ff49ca1977ec114f88c321706d57b9f15cd6c03b
“Leonardo”3310 <jl.rangel@uniandes.edu.co>
2024-04-12 09:12:25 -0500

    xd

--- measurements/logic/logic_measurement.py
+++ measurements/logic/logic_measurement.py
@@ -5,7 +5,12 @@ def get_measurements():
     return (queryset)
 
 def create_measurement(form):
-    measurement = form.save()
-    measurement.value +=1
-    measurement.save()
+    last_measurement = Measurement.objects.last()
+    new_value = 1  # valor predeterminado si no existen mediciones
+    if last_measurement:
+        new_value = last_measurement.value + 1
+
+    new_measurement = form.save()
+    new_measurement.value = new_value
+    new_measurement.save()
     return ()
